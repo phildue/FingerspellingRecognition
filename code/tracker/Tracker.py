0e09849febf137b015e8050fdf2b8d260e4e4a0c
phil <phild@protonmail.com>
2017-05-24 22:13:13 +0200

    added mean shift filtering TODO: try background subtraction/ edge detection

--- code/tracker/Tracker.py
+++ code/tracker/Tracker.py
@@ -1,11 +1,22 @@
 import numpy as np
 import cv2
 
-# Load an color image in grayscale
-dir_rsrc = '../../resource/dataset/'
-dir_a = 'fingerspelling5/dataset5/A/a/'
-test_a = 'color_0_0002.png'
-img = cv2.imread(dir_rsrc + dir_rsrc + dir_a + test_a, 0)
-cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+def mean_shift_segmentation(img):
+    return cv2.pyrMeanShiftFiltering(img, 32, 32)
+
+def main():
+    # Load an color image in grayscale
+    dir_rsrc = '../../resource/dataset/'
+    dir_a = 'fingerspelling5/dataset5/A/a/'
+    test_a = 'color_0_0002.png'
+    img = cv2.imread(dir_rsrc + dir_rsrc + dir_a + test_a, 1)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    img = mean_shift_segmentation(img)
+    cv2.imshow('image', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+main()

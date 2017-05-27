ebdb0f9c5153b98e59c496a74dc6555bb032461a
phil <phild@protonmail.com>
2017-05-27 19:45:23 +0200

    added padding

--- code/tracker/MeanShiftSegmentor.py
+++ code/tracker/MeanShiftSegmentor.py
@@ -4,53 +4,79 @@ import cv2
 from numpy import array, zeros, linalg, exp, floor
 
 
-class MeanShiftSegmentor:
+def mean_shift_segmentation(image, kernel_size=3, bandwidth=16, max_iterations=1000):
+    segmentor = MeanShiftSegmentor(image, kernel_size, bandwidth)
+
+    for i in range(0, max_iterations):
+        limits = image.shape
+        image_prev = segmentor.image
+        for x in range(0, limits[0]):
+            for y in range(0, limits[1]):
+                window = segmentor.neighbours([x, y])
+                segmentor.image[x + segmentor.kernel_dist, y + segmentor.kernel_dist] = segmentor.weighted_sum(window)
+
+        if linalg.norm(image_prev - segmentor.image) < 0.0000001:
+            break
+
+    return segmentor.remove_padding(segmentor.image)
+
+
+class MeanShiftSegmentor(object):
     image = array
     kernel_size = int
     kernel = array
+    kernel_dist = int
 
-    def segment(self, image, kernel_size=3, bandwidth=16, max_iterations=100):
-        self.image = image
+    def __init__(self, image: array, kernel_size: int, bandwidth: int):
+
+        self.kernel_dist = int(floor(float(kernel_size) / 2))
+        self.image = self.__border_padding(image)
+        cv2.imshow('image', self.image)
+        cv2.waitKey(0)
         self.kernel_size = kernel_size
         self.kernel = self.__create_kernel(kernel_size, bandwidth)
 
-        for i in range(0, max_iterations):
-            x_max, y_max = self.image.shape
-            image_prev = image
-            for x in range(0, x_max):
-                for y in range(y_max):
-                    window = self.__neighbours(self.image[x, y])
-                    self.image[x, y] = self.__weighted_sum(window)
-
-            if linalg.norm(image_prev - image) < 0.0000001:
-                break
-
-        return image
-
-    def __weighted_sum(self, window: array):
+    def weighted_sum(self, window: array):
         weighted_sum = 0
-        x_max, y_max = window.shape
-        for x in range(0, x_max):
-            for y in range(y_max):
+        limits = window.shape
+        for x in range(0, limits[0]):
+            for y in range(limits[1]):
                 weighted_sum += self.kernel[x, y] * window[x, y]
         return weighted_sum
 
-    def __neighbours(self, point):
-        x = point(1)
-        y = point(2)
-        return self.image[x - 1:x + 1, y - 1:y + 1]
+    def neighbours(self, point):
+        x = point[0]
+        y = point[1]
+        return self.image[x - self.kernel_dist:x + self.kernel_dist, y - self.kernel_dist:y + self.kernel_dist]
 
-    def __create_kernel(self, kernel_size, bandwidth):
-        mat = zeros(kernel_size)
-        mean = zeros(2, 1)
-        mean[:] = floor(kernel_size / 2)
+    def __create_kernel(self, kernel_size: int, bandwidth: int):
+        mat = zeros(shape=(kernel_size, kernel_size))
+        mean = zeros(shape=(2, 1))
+        mean[:] = self.kernel_dist
 
         for x in range(0, kernel_size):
             for y in range(0, kernel_size):
-                mat = self.__gauss([x, y], mean, bandwidth)
+                mat[x, y] = self.__gauss([x, y], mean, bandwidth)
 
         return mat / linalg.norm(mat)
 
     def __gauss(self, point, mean, bandwidth) -> float:
-        distance = mean - point
+        distance = linalg.norm(mean - point)
         return exp(-0.5 * (distance / bandwidth) ** 2)
+
+    def __border_padding(self, image):
+        limits = image.shape
+        image_padded = zeros(shape=(limits[0] + self.kernel_dist * 2, limits[1] + self.kernel_dist * 2, 3))
+        image_padded[self.kernel_dist:limits[0] + self.kernel_dist, self.kernel_dist:limits[1] + self.kernel_dist,
+        :] = image[:, :, :]
+        #image_padded = image_padded.astype(int)
+        for i in range(0, self.kernel_dist):
+            image_padded[i, :, :] = image_padded[self.kernel_dist, :, :]
+            image_padded[limits[0] + i, :, :] = image_padded[limits[0], :, :]
+            image_padded[:, i, :] = image_padded[:, self.kernel_dist, :]
+            image_padded[:, limits[1] + i, :] = image_padded[:, limits[1], :]
+        return image_padded
+
+    def remove_padding(self, image):
+        return image[self.kernel_dist:image.shape[0] - self.kernel_dist,
+               self.kernel_dist:image.shape[1] - self.kernel_dist]
--- code/tracker/Tracker.py
+++ code/tracker/Tracker.py
@@ -1,18 +1,16 @@
-import numpy as np
 import cv2
 
+from tracker.MeanShiftSegmentor import mean_shift_segmentation
 
-def mean_shift_segmentation(img):
-    return cv2.pyrMeanShiftFiltering(img, 32, 32)
 
 def main():
-    # Load an color image in grayscale
+    # Load an color image in colour
     dir_rsrc = '../../resource/dataset/'
     dir_a = 'fingerspelling5/dataset5/A/a/'
     test_a = 'color_0_0002.png'
     img = cv2.imread(dir_rsrc + dir_rsrc + dir_a + test_a, 1)
     cv2.imshow('image', img)
-    cv2.waitKey(0)
+    cv2.waitKey(500)
     img = mean_shift_segmentation(img)
     cv2.imshow('image', img)
     cv2.waitKey(0)

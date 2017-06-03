da5c72826db5af8c25ec6948a5b5254cd8ee0b87
phil <phild@protonmail.com>
2017-06-04 00:32:22 +0200

    renaming

--- code/classification/ClassifierTest.py
+++ code/classification/ClassifierTest.py
@@ -1,16 +1,14 @@
-from sklearn.decomposition import PCA
-from sklearn.model_selection import train_test_split
-
-from classifier.ClassifierClass import Classifier
+from classification.ClassifierClass import Classifier
 from evaluation.hold_out import hold_out_eval
 from feature_generation.DataGenerator import gendata
+from feature_generation.FeatureExtraction import pca_transform
 
 
 def main():
     dir_dataset = '../../resource/dataset/fingerspelling5/dataset5/A/'
     n_data = 100
     data, labels = gendata(dir_dataset, n_data)
-
+    data = pca_transform(data)
     error = hold_out_eval(Classifier(), data, labels)
 
     print("Error: " + str(error))
--- code/feature_generation/FeatureExtraction.py
+++ code/feature_generation/FeatureExtraction.py
@@ -1,6 +1,7 @@
 from sklearn.decomposition import PCA
 
 
-def pca_transform(data):
-    pca = PCA(n_components=0.9, svd_solver="full")
+def pca_transform(data, n_pc=0.9):
+    # default 90% of variance is preserved
+    pca = PCA(n_pc, svd_solver="full")
     return pca.fit_transform(data)

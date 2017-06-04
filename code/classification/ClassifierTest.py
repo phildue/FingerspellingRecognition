e507285ce1dd5c819204235a21be94b90a44ae53
phil <phild@protonmail.com>
2017-06-04 21:02:53 +0200

    DatasetGenerator now selects from all folders at random

--- code/classification/ClassifierTest.py
+++ code/classification/ClassifierTest.py
@@ -6,11 +6,12 @@ from feature_generation.FeatureExtraction import pca_transform
 
 
 def main():
-    dir_dataset = '../../resource/dataset/fingerspelling5/dataset5/A/'
-    n_data = 5
+    dir_dataset = '../../resource/dataset/fingerspelling5/dataset5/'
+    n_data = 100
     data, labels = gendata(dir_dataset, n_data)
-    #data = get_dissim_rep(data)
     data = pca_transform(data)
+    data = get_dissim_rep(data)
+
     error = hold_out_eval(Classifier(), data, labels)
 
     print("Error: " + str(error))
--- code/feature_generation/DataGenerator.py
+++ code/feature_generation/DataGenerator.py
@@ -1,26 +1,31 @@
 import os
+import random
 
 import cv2
 import numpy as np
 
 
-def gendata(dir_dataset, n_samples_selected):
+def gendata(dir_dataset, sample_size):
+    sets = ["A", "B", "C", "D", "E"]
     alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v",
                 "w", "x", "y"]
     n_letters = len(alphabet)
     im_res = (100, 120, 3)
     dim = im_res[0] * im_res[1] * im_res[2]
-    data = np.zeros(shape=(n_samples_selected * n_letters, dim))
-    labels = np.zeros(shape=(n_samples_selected * n_letters, 1))
-    for class_, directory in enumerate(alphabet, 1):
-        fnames = os.listdir(dir_dataset + directory)
-        fnames = [f for f in fnames if 'color' in f]
-        for n_sample, filename in enumerate(fnames):
-            if n_sample >= n_samples_selected:
-                break
-            img = cv2.imread(dir_dataset + directory + "/" + filename, 1)
+    data = np.zeros(shape=(sample_size * n_letters, dim))
+    labels = np.zeros(shape=(sample_size * n_letters, 1))
+
+    for class_, dir_letter in enumerate(alphabet, 1):
+        paths = []
+        for dir_set in sets:
+            fnames = [f for f in os.listdir(dir_dataset + dir_set + "/" + dir_letter) if 'color' in f]
+            for fname in fnames:
+                paths.append(dir_dataset + dir_set + "/" + dir_letter + "/" + fname)
+
+        for sel_samples, path in enumerate(random.sample(paths, sample_size)):
+            img = cv2.imread(path, 1)
             img = cv2.resize(img, (im_res[0], im_res[1]))
-            index = n_sample + (class_ - 1) * n_samples_selected
+            index = sel_samples + (class_ - 1) * sample_size
             data[index, :] = img.reshape(1, dim)
             labels[index] = class_
 
@@ -28,7 +33,7 @@ def gendata(dir_dataset, n_samples_selected):
 
 
 def main():
-    dir_dataset = '../../resource/dataset/fingerspelling5/dataset5/A/'
+    dir_dataset = '../../resource/dataset/fingerspelling5/dataset5/'
     n_data = 10
     data, labels = gendata(dir_dataset, n_data)
     print("Data:\n")
@@ -36,4 +41,5 @@ def main():
     print("Labels:\n")
     print(labels)
 
-# main()
+
+#main()

e1b4b303cab3ccba105845ecb203da93ccc976e0
phil <phild@protonmail.com>
2017-06-06 22:21:05 +0200

    random test pic selection

--- code/daq/preprocessing/PreProcessingTest.py
+++ code/daq/preprocessing/PreProcessingTest.py
@@ -1,24 +1,26 @@
 import cv2
 
 # from tracker.MeanShiftSegmentor import mean_shift_segmentation
+from daq.DatasetGenerator import gendata
 from daq.ImReader import read_im_file
 from daq.preprocessing.PreProcessing import pre_processing
 
 
 def main():
-    # Load an color image in colour
-    dir_rsrc = '../../../resource/dataset/'
-    dir_a = 'fingerspelling5/dataset5/E/c/'
-    sample_image = 'color_2_0192.png'
-    img = read_im_file(dir_rsrc + dir_a + sample_image)
+
+    img, _ = gendata(dir_dataset='../../../resource/dataset/fingerspelling5/dataset5/',
+                     sample_size=1,
+                     sets=["E"])
+
+    img.reshape(shape=(100, 120))
 
     cv2.imshow('image', img)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     img = pre_processing(img)
     cv2.imshow("after preprocessing", img)
     cv2.waitKey(10000)
-
     cv2.destroyAllWindows()
+    exit(0)
 
 
 main()

dafd0eaaf3e097dd7e11f87a4a4843189082fa2a
phil <phild@protonmail.com>
2017-06-06 11:45:09 +0200

    moved skin segmentor to preprocessing

--- code/daq/preprocessing/PreProcessing.py
+++ code/daq/preprocessing/PreProcessing.py
@@ -1,11 +1,28 @@
 import cv2
-from numpy import shape
-
-from daq.preprocessing.SkinSegmentor import filter_skin
+import numpy as np
 
 
-def pre_processing(img, im_size = (100, 120)):
+def pre_processing(img, im_size=(100, 120)):
     img_preprocessed = cv2.resize(img, (im_size[0], im_size[1]))
     img_preprocessed = filter_skin(img_preprocessed)
-    #img_preprocessed = cv2.cvtColor(img_preprocessed, cv2.COLOR_RGB2GRAY)
     return img_preprocessed
+
+
+def filter_skin(image, lower_thresh=np.array([0, 80, 80], dtype="uint8"),
+                upper_thresh=np.array([20, 170, 170], dtype="uint8")):
+    converted = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    skin_mask = cv2.inRange(converted, lower_thresh, upper_thresh)
+    skin_mask = cv2.GaussianBlur(skin_mask, (3, 3), 2)
+
+    # apply a series of erosions and dilations to the mask
+    # using an elliptical kernel
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
+    skin_mask = cv2.erode(skin_mask, kernel, iterations=1)
+    skin_mask = cv2.dilate(skin_mask, kernel, iterations=1)
+
+    # blur the mask to help remove noise, then apply the
+    # mask to the frame
+    skin_mask = cv2.GaussianBlur(skin_mask, (3, 3), 2)
+    skin = cv2.bitwise_and(image, image, mask=skin_mask)
+
+    return skin
--- code/daq/preprocessing/PreProcessingTest.py
+++ code/daq/preprocessing/PreProcessingTest.py
@@ -2,30 +2,31 @@ import cv2
 
 # from tracker.MeanShiftSegmentor import mean_shift_segmentation
 from daq.ImReader import read_im_file
+from daq.preprocessing.PreProcessing import pre_processing
 from daq.preprocessing.SkinSegmentor import filter_skin
 
 
 def main():
     # Load an color image in colour
     dir_rsrc = '../../../resource/dataset/'
-    dir_a = 'fingerspelling5/dataset5/A/c/'
-    sample_image = 'color_2_0002.png'
+    dir_a = 'fingerspelling5/dataset5/E/c/'
+    sample_image = 'color_2_0192.png'
     img = read_im_file(dir_rsrc + dir_a + sample_image)
 
     # cv2.imshow('image', img)
     # cv2.waitKey(0)
-    skin = filter_skin(img)
-    cv2.imshow("skin", skin)
+    img = pre_processing(img)
+    cv2.imshow("skin", img)
     cv2.waitKey(10000)
-    skin = cv2.cvtColor(skin, cv2.COLOR_RGB2GRAY)
-    ## Reduce
-    skin = cv2.blur(skin, (7, 7))
-    ## Canny
-    skin = cv2.Canny(skin, threshold1=50, threshold2=150)
-    _, skin = cv2.threshold(skin, 50, 255, cv2.THRESH_BINARY)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # ## Reduce
+    img = cv2.blur(img, (7, 7))
+    # ## Canny
+    img = cv2.Canny(img, threshold1=20, threshold2=100)
+    _, img = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
     # show the skin in the image along with the mask
     # cv2.imshow("detected skin", np.hstack([img, skin]))
-    cv2.imshow("skin binary ", skin)
+    cv2.imshow("skin binary ", img)
     cv2.waitKey(10000)
     cv2.destroyAllWindows()
 

68e9babc9fadb7aa124c302d29cacd3d820563e9
phil <phild@protonmail.com>
2017-06-07 14:33:56 +0200

    skinclassifier stores trained object now

--- code/classification/SkinClassifier.py
+++ code/classification/SkinClassifier.py
@@ -1,12 +1,22 @@
+from __builtin__ import file
+from sklearn.externals import joblib
 from sklearn.neighbors import KNeighborsClassifier
 
+from daq.datageneration.DatasetGeneratorSkin import gendata_skin
 from exceptions.NotTrained import NotTrained
 
 
 class SkinClassifier:
-
     scikit_object = KNeighborsClassifier(n_neighbors=2, algorithm='ball_tree')
     trained = False
+
+    def __init__(self, trainedc_path='trained_skin_classifier.pkl'):
+        if file.exists(trainedc_path):
+            self.scikit_object = joblib.load(trainedc_path)
+        else:
+            data, labels = gendata_skin()
+            self.train(data, labels)
+            joblib.dump(self.scikit_object, trainedc_path)
 
     def train(self, data, labels):
         self.scikit_object.fit(data, labels.ravel())
@@ -17,6 +27,3 @@ class SkinClassifier:
             raise NotTrained
         else:
             return self.scikit_object.predict(data)
-
-
-

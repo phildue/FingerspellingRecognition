8767b73acc040feb8565d738bea46a569045daad
phil <phild@protonmail.com>
2017-06-17 17:48:47 +0200

    started with calculating equally distributed points along the shape of the hand

--- code/daq/preprocessing/PreProcessing.py
+++ code/daq/preprocessing/PreProcessing.py
@@ -24,7 +24,7 @@ def extract_descriptor(img):
     img = prefilter(img)
     contour = get_longest_contours(img)[0]
     centroid = get_centroid(contour)
-    return get_centroid_distances(contour, centroid)
+    return get_centroid_distances(contour, centroid, n_points=50)
 
 
 def get_centroid(contour):
@@ -32,22 +32,46 @@ def get_centroid(contour):
     return np.array([moms['m01'] / moms['m00'], moms['m10'] / moms['m00']])
 
 
-def get_centroid_distances(contour, centroid):
+def get_centroid_distances(contour, centroid, n_points):
+    # calculate the distance of every point to the centroid
+    #  this results in rotation invariant features
+    points = get_equally_distr_points(contour, n_points)
+
     distances = []
-    for p in contour:
-        distances.append(linalg.norm(centroid - p))
+    for p in points:
+        distances.append(linalg.norm(p - centroid))
 
     return distances
 
 
+def get_equally_distr_points(contour, n):
+    # get points along the found contour in equally distributed
+    # distances. This is required to have the same amount of
+    # features for every shape
+    total_c_dist = np.linalg.norm(contour, axis=0)
+    delta_dist = total_c_dist / n
+    points = []
+    for i in range(0, n):
+        p_dist = 0
+        n = 0
+        while p_dist < delta_dist * i:
+            n += 1
+            p_dist = linalg.norm(contour[0:n])
+        overshoot_dist = linalg.norm(contour[0:n]) - delta_dist * i
+        total_dist = linalg.norm(contour(n) - contour(n - 1))
+        required_dist = 1 - overshoot_dist / total_dist
+        direction = contour(n) - contour(n - 1)
+        points.append(contour(n - 1) + direction * required_dist)
+    return points
+
+
 def prefilter(img, im_size=(100, 120), roi_size=(30, 30)):
+    # find roi (hand), crop it, find edges
     img_resized = cv2.resize(img, im_size)
 
     roi = get_roi(img_resized)
     roi = cv2.resize(roi, roi_size)
     roi = cv2.Canny(roi, threshold1=50, threshold2=100)
-    # _, roi = cv2.threshold(roi, 1, 255, cv2.THRESH_BINARY)
-    # contour_img = np.zeros(roi.shape, np.uint8)
 
     return roi
 

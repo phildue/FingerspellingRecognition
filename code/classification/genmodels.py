887ba2f10ed6d5f0ff8c59efb6b227edd4fa2893
phil <phild@protonmail.com>
2017-06-20 15:35:51 +0200

    main script working

--- code/classification/genmodels.py
+++ code/classification/genmodels.py
@@ -1,22 +1,20 @@
+from sklearn.decomposition import PCA
 from sklearn.externals import joblib
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
 
-from classification.SignClassifier import SignClassifier
 from daq.DatasetGenerator import gendata_sign
 from daq.ImReader import get_paths_tm
-from representation.FeatureExtraction import get_scaler, get_extractor
 
 dir_dataset = '../../resource/dataset/tm'
-scaler_file = '../../resource/models/scaler.pkl'
-extractor_file = '../../resource/models/extractor.pkl'
-classif_file = '../../resource/models/classif.pkl'
+model_file = '../../resource/models/model.pkl'
 # Reads dataset, trains models and stores them on file system
 data, labels = gendata_sign(get_paths_tm(dir_dataset=dir_dataset), 2000)
-scaler = get_scaler(data)
-data = scaler.transform(data)
-extractor = get_extractor(data)
-data = extractor.transform(data)
-classif = SignClassifier()
-classif.train(data, labels)
-joblib.dump(scaler, scaler_file)
-joblib.dump(extractor, scaler_file)
-joblib.dump(classif.scikit_object, classif_file)
+
+scaler = StandardScaler()
+extractor = PCA(0.9, svd_solver="full")
+classifier = SVC()
+pipe = Pipeline(steps=[('scaler', scaler), ('pca', extractor), ('clf', classifier)])
+pipe.fit(data, labels.ravel())
+joblib.dump(pipe, model_file)
--- code/main/main.py
+++ code/main/main.py
@@ -1,36 +1,36 @@
 import random
 
-import numpy as np
+import cv2
+from sklearn.externals import joblib
 
-from classification.SignClassifier import SignClassifier
 from daq.ImReader import read_image
 from daq.preprocessing.PreProcessing import preprocess, extract_descriptor
 
 # init
-from representation.FeatureExtraction import extract_features
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
            "u",
            "v", "w", "x", "y"]
 
 dir_dataset = '../../resource/dataset/tm'
-scaler_file = '../../resource/models/scaler.pkl'
-extractor_file = '../../resource/models/extractor.pkl'
-classif_file = '../../resource/models/classif.pkl'
+model = joblib.load('../../resource/models/model.pkl')
+
 cmd = 'y'
 while cmd != 'n':
     example_image_file = "../../resource/dataset/tm/" + str(random.choice(letters)) + str(
         random.choice(range(1, 40))) + ".tif"
     # read image
     img = read_image(example_image_file)
+    #
+    cv2.imshow("Read image",img)
     # crop hand
     img = preprocess(img)
     # extract descriptor
     descriptor = extract_descriptor(img)
-    # apply preprocessing
-    object_rep = extract_features(descriptor, scaler_file, extractor_file)
     # classify
-    class_ = SignClassifier().classify(object_rep, classif_file)
+    class_ = model.predict(descriptor)
     # print output
-    print("Letter " + str(letters[class_]))
-    cmd = input("Continue? [y|n]: ")
+    print("Letter " + str(letters[int(class_)]))
+    cv2.waitKey(0)
+
+    # cmd = input("Continue? [y|n]: ")

f117794fbcd5a9036399192169fbd0ff0ccc7888
phil <phild@protonmail.com>
2017-06-20 16:54:24 +0200

    back to reading files form file system

--- code/main/main.py
+++ code/main/main.py
@@ -14,25 +14,16 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o",
 
 dir_dataset = '../../resource/dataset/tm'
 model = joblib.load('../../resource/models/model.pkl')
-cap = cv2.VideoCapture()
-if not cap.isOpened:
-    print("Could not read webcam")
 
-while cap.isOpened and cv2.waitKey(10) != 27:
-    # letter = str(random.choice(letters))
-    # example_image_file = "../../resource/dataset/tm/" + letter + str(
-    #    random.choice(range(1, 40))) + ".tif"
+cmd = 'y'
+while cmd != 'n':
+    letter = str(random.choice(letters))
+    example_image_file = "../../resource/dataset/tm/" + letter + str(
+        random.choice(range(1, 40))) + ".tif"
     # read image
-    # img = read_image(example_image_file)
-    success, img = cap.read()
-    if not success:
-        print("Error on reading webcam")
-        break
-
-    cv2.imshow("Read image", img)
-
-    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    img = read_image(example_image_file)
     #
+    cv2.imshow("Read image", img)
     # crop hand
     try:
         img = preprocess(img)
@@ -45,5 +36,6 @@ while cap.isOpened and cv2.waitKey(10) != 27:
     # print output
     print("Detected Letter " + str(letters[int(class_) - 1]))
     print("Actual Letter: " + letter)
+    cv2.waitKey(0)
 
-cv2.destroyAllWindows()
+    # cmd = input("Continue? [y|n]: ")

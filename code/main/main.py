60a9dfc8be205ef174bc691c3543b7a0e782f84c
phil <phild@protonmail.com>
2017-06-20 15:49:13 +0200

    fixed index bug

--- code/main/main.py
+++ code/main/main.py
@@ -7,6 +7,7 @@ from daq.ImReader import read_image
 from daq.preprocessing.PreProcessing import preprocess, extract_descriptor
 
 # init
+from exceptions.exceptions import NoRoiFound
 
 letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t",
            "u",
@@ -17,20 +18,25 @@ model = joblib.load('../../resource/models/model.pkl')
 
 cmd = 'y'
 while cmd != 'n':
-    example_image_file = "../../resource/dataset/tm/" + str(random.choice(letters)) + str(
+    letter = str(random.choice(letters))
+    example_image_file = "../../resource/dataset/tm/" + letter + str(
         random.choice(range(1, 40))) + ".tif"
     # read image
     img = read_image(example_image_file)
     #
     cv2.imshow("Read image",img)
     # crop hand
-    img = preprocess(img)
+    try:
+        img = preprocess(img)
+    except NoRoiFound:
+        continue
     # extract descriptor
     descriptor = extract_descriptor(img)
     # classify
     class_ = model.predict(descriptor)
     # print output
-    print("Letter " + str(letters[int(class_)]))
+    print("Detected Letter " + str(letters[int(class_)-1]))
+    print("Actual Letter: " + letter)
     cv2.waitKey(0)
 
     # cmd = input("Continue? [y|n]: ")
